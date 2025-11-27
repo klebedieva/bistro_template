102e772a66a4089259995ad3e1536904dfe37cff
karyna.lebedieva <karyna.lebedieva@gmail.com>
2025-11-27 10:58:59 +0100

    feat: add real legal pages and link them from footer

---
+++ src/Controller/MentionsLegalesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Mentions légales controller.
+ */
+class MentionsLegalesController extends AbstractController
+{
+    #[Route('/mentions-legales', name: 'app_mentions_legales')]
+    public function __invoke(): Response
+    {
+        return $this->render('pages/mentions-legales.html.twig', [
+            'seo_title' => 'Mentions légales | Le Trois Quarts',
+            'seo_description' => 'Informations légales, éditeur du site, responsable de publication et hébergeur du Trois Quarts.',
+            'seo_og_description' => 'Mentions légales et informations réglementaires du Trois Quarts.',
+            'seo_robots' => 'noindex,follow',
+        ]);
+    }
+}
+
+
---
+++ src/Controller/PrivacyPolicyController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Politique de confidentialité (RGPD) controller.
+ */
+class PrivacyPolicyController extends AbstractController
+{
+    #[Route('/politique-confidentialite', name: 'app_privacy_policy')]
+    public function __invoke(): Response
+    {
+        return $this->render('pages/privacy-policy.html.twig', [
+            'seo_title' => 'Politique de confidentialité | Le Trois Quarts',
+            'seo_description' => 'Découvrez comment Le Trois Quarts collecte, utilise et protège vos données personnelles.',
+            'seo_og_description' => 'Politique de confidentialité et informations RGPD du Trois Quarts.',
+            'seo_robots' => 'noindex,follow',
+        ]);
+    }
+}
+
+
