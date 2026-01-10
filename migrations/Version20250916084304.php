0a8f186c304b90c4519267d29de2ec51adbc1166
karyna.lebedieva <karyna.lebedieva@gmail.com>
2026-01-10 21:12:43 +0100

    Fix Version20250916084304: Remove COMMENT from ALTER TABLE ADD for PostgreSQL - Replace COMMENT in ALTER TABLE with separate COMMENT ON COLUMN - Convert TINYINT(1) to BOOLEAN - Convert DATETIME to TIMESTAMP(0) WITHOUT TIME ZONE - Convert LONGTEXT to TEXT

--- migrations/Version20250916084304.php
+++ migrations/Version20250916084304.php
@@ -19,13 +19,25 @@ final class Version20250916084304 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE reservations ADD status VARCHAR(20) NOT NULL, ADD is_confirmed TINYINT(1) NOT NULL, ADD confirmed_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD confirmation_message LONGTEXT DEFAULT NULL');
+        $platform = $this->connection->getDatabasePlatform();
+        $isPostgres = $platform instanceof \Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+
+        if ($isPostgres) {
+            $this->addSql('ALTER TABLE reservations ADD status VARCHAR(20) NOT NULL, ADD is_confirmed BOOLEAN NOT NULL, ADD confirmed_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, ADD confirmation_message TEXT DEFAULT NULL');
+            $this->addSql('COMMENT ON COLUMN reservations.confirmed_at IS \'(DC2Type:datetime_immutable)\'');
+        } else {
+            $this->addSql('ALTER TABLE reservations ADD status VARCHAR(20) NOT NULL, ADD is_confirmed TINYINT(1) NOT NULL, ADD confirmed_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD confirmation_message LONGTEXT DEFAULT NULL');
+        }
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        $platform = $this->connection->getDatabasePlatform();
+        $isPostgres = $platform instanceof \Doctrine\DBAL\Platforms\PostgreSQLPlatform;
+
+        if ($isPostgres) {
+            $this->addSql('COMMENT ON COLUMN reservations.confirmed_at IS NULL');
+        }
         $this->addSql('ALTER TABLE reservations DROP status, DROP is_confirmed, DROP confirmed_at, DROP confirmation_message');
     }
 }
